8fa92fce8753ed7efeaad2742911ea94b3ba07c7
Fahad <diary1234590@gmail.com>
2024-05-15 00:55:14 +0500

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+var bulb = document.getElementById("bulb")
+
+btn_on.addEventListener("click", function(){
+
+    bulb.src = "on.png" 
+}
+);
+
+btn_off.addEventListener("click", function(){
+
+    bulb.src = "off.png" 
+}
+);
